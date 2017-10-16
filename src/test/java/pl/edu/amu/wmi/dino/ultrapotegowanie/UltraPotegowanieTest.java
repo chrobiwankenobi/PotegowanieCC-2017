b7924b6d693722c6048642e21959b15bb2bfa53c
chrobiwankenobi <xchrobi@gmail.com>
2017-10-17 01:51:33 +0200

    Update UltraPotegowanieTest.java

--- src/test/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanieTest.java
+++ src/test/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanieTest.java
@@ -122,8 +122,8 @@ public class UltraPotegowanieTest {
         String start = "10000000";
         String wynik = "0.000000000001";
         for(int i=0;i<10;i++){
-            start += "1";
-            wynik = wynik.replace('1', '0');
+            start += "0";
+            wynik = wynik.replace('0', '1');
             wynik += "01";
             assertEquals(wynik, UltraPotegowanie.poteguj(start, "-1"));
         }
@@ -137,7 +137,7 @@ public class UltraPotegowanieTest {
     
     @Test
     public void testNotacjaNaukowa2(){
-        assertEquals("0.00125",UltraPotegowanie.poteguj("2e3","-3"));
+        assertEquals("0.00125",UltraPotegowanie.poteguj("2e3","-2"));
     }
   
      /*

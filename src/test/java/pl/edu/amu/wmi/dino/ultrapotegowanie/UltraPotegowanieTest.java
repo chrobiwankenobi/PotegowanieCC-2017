2d484b6ddbd5f17d8605a68780b4b61d49e1aab7
chrobiwankenobi <xchrobi@gmail.com>
2017-10-17 02:37:17 +0200

    Update UltraPotegowanieTest.java

--- src/test/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanieTest.java
+++ src/test/java/pl/edu/amu/wmi/dino/ultrapotegowanie/UltraPotegowanieTest.java
@@ -166,7 +166,7 @@ public class UltraPotegowanieTest {
            // Kuba Chrobot
     @Test
     public void testPoteguja() {
-        assertEquals("123456789", UltraPotegowanie.poteguj("12345", "6789"));
+        assertEquals("123456789", UltraPotegowanie.poteguj("25", "-2"));
     }
     
     @Test
